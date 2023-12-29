1104652bbc6fc553d317b0f05f5df36ab43aac18
pradosh-arduino <pradoshnathan@gmail.com>
2023-12-29 16:00:52 +0530

    Added font to character tool, improved readme.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# tools
+### Tools of FrostWing OS
+Welcome this repository contains tools which are to be run in the host computer.
+
+#### Overview of tools in this repository
+
+1. **font-to-char.py** - this converts the standard VGA font to character arrays which are used as the fonts for the OS's terminal. *used at [unifont.h](https://github.com/Frost-Wing/osdev/blob/main/source/includes/unifont.h)*
---
+++ font-to-char.py
@@ -0,0 +1,24 @@
+"""
+    This python code was created by pradosh-arduino 
+    and when you use this code you must credit me.
+"""
+
+import sys
+
+file_path = sys.argv[1]  # Replace the path to a diffrent one if needed.
+
+try:
+    with open(file_path, 'rb') as file:
+        byte_count = 0
+        byte = file.read(1)
+        while byte:
+            print(f'0x{byte.hex()}', end=", ")
+            byte_count += 1
+            if byte_count % 16 == 0:
+                print()  # Start a new line every 16 bytes
+                print("    ", end="")
+            byte = file.read(1)
+except FileNotFoundError:
+    print(f"File '{file_path}' not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
